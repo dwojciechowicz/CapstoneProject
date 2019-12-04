81904c2090dadbd62a0af4d1164bde2b6b495212
Zosiaa <zmatyjewska@wp.pl>
2019-12-04 17:48:14 +0800

    added login and register activity(not working)

---
+++ KNUpay/app/src/main/java/com/example/knupay10/Loginin.java
@@ -0,0 +1,21 @@
+package com.example.knupay10;
+
+
+
+import android.os.Bundle;
+import android.content.Intent;
+//import com.google.android.material.floatingactionbutton.FloatingActionButton;
+//import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+
+public class Loginin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginin);
+    }
+}
