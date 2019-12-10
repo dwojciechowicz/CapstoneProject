e5ceb8cc1d215e1d7b4c529ded77d7c4bd008c7a
Zosiaa <zmatyjewska@wp.pl>
2019-12-10 14:51:14 +0800

    Fixing a few errors -> Working version
    Fixed small error with the strings and files synchronization

--- KNUpay/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ KNUpay/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f070041;
-        public static final int end = 0x7f070057;
-        public static final int gone = 0x7f07005b;
-        public static final int invisible = 0x7f070066;
-        public static final int left = 0x7f070068;
-        public static final int packed = 0x7f070078;
-        public static final int parent = 0x7f070079;
-        public static final int percent = 0x7f07007b;
-        public static final int right = 0x7f07007f;
-        public static final int spread = 0x7f070097;
-        public static final int spread_inside = 0x7f070098;
-        public static final int start = 0x7f07009d;
-        public static final int top = 0x7f0700b4;
-        public static final int wrap = 0x7f0700bb;
+        public static final int end = 0x7f07005e;
+        public static final int gone = 0x7f070062;
+        public static final int invisible = 0x7f07006d;
+        public static final int left = 0x7f07006f;
+        public static final int packed = 0x7f07007f;
+        public static final int parent = 0x7f070080;
+        public static final int percent = 0x7f070082;
+        public static final int right = 0x7f070086;
+        public static final int spread = 0x7f07009e;
+        public static final int spread_inside = 0x7f07009f;
+        public static final int start = 0x7f0700a4;
+        public static final int top = 0x7f0700bc;
+        public static final int wrap = 0x7f0700c3;
     }
     public static final class styleable {
         private styleable() {}
